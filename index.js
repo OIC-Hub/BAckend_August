240ef55572d6e47b9b7233ae3efdd77c042f9276
Adefokun Adeoluwa Israel <adefokunadeoluwaisrael@gmail.com>
2026-08-25 10:31:37 +0100

    Day 2 backend

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const fs = require('fs')
+// const fs = require('fs')
+const fs = require("fs/promises");
 
-fs.readFile("./class.txt", (err, data) => {
-    if (err) throw err;
-    console.log(data);
-})
+// fs.readFile("./class.txt", (err, data) => {
+//     if (err) throw err;
+//     console.log(data);
+// })
 
 // fs.readFile('./message.txt',  {
 //   if (err) throw err
@@ -11,8 +12,38 @@ fs.readFile("./class.txt", (err, data) => {
 // })
 
 
-console.log("Backend class")
+// console.log("Backend class")
 
-console.log(__dirname)
-console.log(__filename)
+// console.log(__dirname)
+// console.log(__filename)
 
+async function ReadDoc(){
+    console.log("Funtion working")
+    const readFile = await fs.readFile("./class.txt", 'utf8')
+    console.log(readFile);
+}
+
+async function WriteDoc(){
+    await fs.writeFile("./class.txt", "Welcome to class")
+}
+
+async function AppendDoc(){
+      
+    const name = "Your Name"
+
+  const today = new Date().toLocaleDateString('en-US', {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  })
+ 
+
+    await fs.appendFile("./class.txt", ` ${name} -- ${today}`)
+}
+
+
+
+ReadDoc();
+WriteDoc();
+AppendDoc();
