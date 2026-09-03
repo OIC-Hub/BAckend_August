52646374a85ec07ae07d5575692fc6a8db1b875c
Adefokun Adeoluwa Israel <adefokunadeoluwaisrael@gmail.com>
2026-09-03 10:55:54 +0100

    CRUD

--- BD/controller/user.controller.js
+++ BD/controller/user.controller.js
@@ -5,7 +5,7 @@ function addUser(req, res){
      const { name, email } = req.body;
 
     if(!name || !email){
-      return  res.status(403).send("Name and Email is required")
+      return res.status(403).send("Name and Email is required")
     }
 
     const newData = {
@@ -25,4 +25,68 @@ function getUsers(req, res){
     res.status(200).json({message: "users fetched successfully", Data});
 }
 
-module.exports = {addUser, getUsers};
+function getOneUser(req, res){
+ try {
+   const {id} = req.params
+
+  if(isNaN(id)){
+   return res.status(400).send("Invalid ID")
+  }
+
+  const findOneUser = Data.find(e => e.id === Number(id));
+
+  if(!findOneUser){
+   return res.status(404).send("User not Found")
+  }
+
+  res.status(200).json({message: "user fetched successfully", findOneUser})
+ } catch (error) {
+  console.error(error)
+ }
+}
+
+function updateUser(req, res){
+ try {
+   const {id} = req.params
+  const {name} = req.body
+
+   if(isNaN(id)){
+   return res.status(400).send("Invalid ID")
+  }
+
+  const findAndUpdate = Data.find(e => e.id === Number(id));
+
+ if(!findAndUpdate){
+   return res.status(404).send("User not Found")
+  }
+
+  if(!name){
+    return res.status(400).send("Name is required")
+  }
+
+ findAndUpdate.name = name;
+
+res.status(200).json({message: "user updated successfully", findAndUpdate})
+ } catch (error) {
+  console.error(error)
+ }
+
+}
+
+function removeUser(req, res) {
+ try {
+   const {id } = req.params
+
+    if(isNaN(id)){
+   return res.status(400).send("Invalid ID")
+  }
+
+  Data.filter(e => e.id !== Number(id))
+
+  res.status(200).send("User deleted successfully")
+ } catch (error) {
+  console.error(error)
+ }
+}
+
+module.exports = {addUser, getUsers, getOneUser, updateUser, removeUser};
--- BD/routes/user.route.js
+++ BD/routes/user.route.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const route = express.Router();
-const {addUser, getUsers} = require("../controller/user.controller")
+const {addUser, getUsers, getOneUser, updateUser, removeUser} = require("../controller/user.controller")
 
 route.post("/add", addUser)
 route.get("/get", getUsers)
+route.get("/user/:id", getOneUser )
+route.put("/user/:id", updateUser )
+route.delete("/delete/:id", removeUser)
+
 
 module.exports = route
